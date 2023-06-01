c0db19a484c90aeb616d305683a7302c87a9e529
mdfaysal22 <mdfaysal.freelancer@gmail.com>
2023-06-01 22:59:55 +0600

    Navbar create [Done]

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,27 @@
-import Navbar from "./Components/Shared/Navbar/Navbar";
-
+import Navbar from './Components/Shared/Navbar/Navbar'
 
 function App() {
-
   return (
     <>
       <div className="sticky top-0">
-      <Navbar/> 
+        <Navbar />
       </div>
-      
+
       <div className="h-[1000px]">
-      <h1>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem recusandae, eos culpa nostrum atque odio consequatur temporibus deserunt nam qui ratione quo quod possimus quos minus vel eligendi incidunt, a natus aspernatur. Nemo modi, doloremque voluptas cum voluptatibus esse dicta exercitationem obcaecati voluptatem aut. Alias exercitationem vitae aliquam cum perferendis minus praesentium suscipit, vero eveniet sint ducimus illo nobis, eum, dolores sit aspernatur tenetur dolor inventore. Iste illum officia assumenda quibusdam a cum voluptate, aliquam blanditiis laudantium amet laboriosam praesentium corporis numquam enim quaerat voluptates, distinctio incidunt. Porro modi rem, facere sequi quis fuga, doloribus incidunt perferendis voluptatum iste voluptas.</h1>
+        <h1>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem
+          recusandae, eos culpa nostrum atque odio consequatur temporibus
+          deserunt nam qui ratione quo quod possimus quos minus vel eligendi
+          incidunt, a natus aspernatur. Nemo modi, doloremque voluptas cum
+          voluptatibus esse dicta exercitationem obcaecati voluptatem aut. Alias
+          exercitationem vitae aliquam cum perferendis minus praesentium
+          suscipit, vero eveniet sint ducimus illo nobis, eum, dolores sit
+          aspernatur tenetur dolor inventore. Iste illum officia assumenda
+          quibusdam a cum voluptate, aliquam blanditiis laudantium amet
+          laboriosam praesentium corporis numquam enim quaerat voluptates,
+          distinctio incidunt. Porro modi rem, facere sequi quis fuga, doloribus
+          incidunt perferendis voluptatum iste voluptas.
+        </h1>
       </div>
     </>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 import daisyui from 'daisyui'
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
   plugins: [daisyui],
   daisyui: {
     themes: {
-      
-        mytheme: {
-
-        }
-      
-    }
+      mytheme: {},
+    },
   },
 }
